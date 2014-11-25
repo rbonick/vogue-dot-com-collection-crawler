cfda2d102a9138b326c3d3e65bd734c5cc98afd5
Ryan Bonick <rjbman93@gmail.com>
2014-11-25 17:39:51 -0600

    Added argument reading

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 __author__ = 'rbonick'
 
@@ -10,15 +11,14 @@ class Crawler:
     def __init__(self):
         pass
 
-    def crawl(self, url):
+    # noinspection PyMethodMayBeStatic
+    def crawl(self, collection_url):
         # Get the website html and confirm it was successful
-        response = requests.get(url)
+        response = requests.get(collection_url)
         response.raise_for_status()
 
         # Turn it into soup
         soup = BeautifulSoup(response.text)
-
-        # print(soup.prettify())
 
         # Get the number of images
         num_images = int(soup.find(attrs={"data-reactid": re.compile(".2.2.1.1.1.1.4.1")}).string)
@@ -27,11 +27,12 @@ class Crawler:
         img_urls = []
         for i in range(1, num_images + 1):
             print("On page {0} of {1}".format(i, num_images))
-            outfit_url = url + "/{0}".format(i)
+            outfit_url = collection_url + "/{0}".format(i)
             response = requests.get(outfit_url)
             soup = BeautifulSoup(response.text)
 
-            # While this can find multiple html tags with .stretch-image, the first is always the desired image tag
+            # While this can find multiple html tags with .stretch-image,
+            # the first is always the desired image tag
             img_tag = soup.select(".stretch-image")[0]
             img_urls.append(img_tag["src"])
 
@@ -40,7 +41,14 @@ class Crawler:
 
 if __name__ == "__main__":
     crawler = Crawler()
-    # results = crawler.crawl("http://www.style.com/slideshows/fashion-shows/spring-2015-ready-to-wear/chanel/collection")
-    results = crawler.crawl("http://www.style.com/slideshows/fashion-shows/fall-2014-couture/maison-martin-margiela/collection")
-    for url in results:
-        print(url)
+
+    try:
+        url = sys.argv[1]
+    except IndexError:
+        print("Please use this program in the format: "
+              "'python crawler.py <collection slideshow url>'")
+        url = None
+    if url:
+        results = crawler.crawl(url)
+        for img_url in results:
+            print(img_url)
